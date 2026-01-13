c8aa44c61b61215ba233047b414e5f7b43697b53
Your Name <lgrandiduchethina@gmail.com>
2026-01-13 21:13:46 +0530

    01-vercel setup and nodejs setup.

---
+++ prisma.config.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config({ path: './.env' });
+
+module.exports = {
+  schema: './prisma/schema.prisma',
+  migrations: {
+    path: './prisma/migrations',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+};
